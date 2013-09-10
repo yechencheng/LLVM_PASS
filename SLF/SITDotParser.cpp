37ddb06d39a0334d5ec252ff3279a536ad2d4f82
yechencheng <yechencheng@gmail.com>
2013-09-10 11:07:55 +0800

    Add SITDotParser

---
+++ SLF/SITDotParser.cpp
@@ -0,0 +1,9 @@
+//
+//  SITDotParser.cpp
+//  Project
+//
+//  Created by ycc on 13-9-10.
+//
+//
+
+#include "SITDotParser.h"
---
+++ SLF/SITDotParser.h
@@ -0,0 +1,14 @@
+//
+//  SITDotParser.h
+//  Project
+//
+//  Created by ycc on 13-9-10.
+//
+//
+
+#ifndef __Project__SITDotParser__
+#define __Project__SITDotParser__
+
+#include <iostream>
+
+#endif /* defined(__Project__SITDotParser__) */
